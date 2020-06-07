f55fdc04ff408684877a9f3ae96766a30e28d411
genie381013 <genie.sheu@gmail.com>
2020-06-07 21:55:01 +0800

    moving methods to Controller.java

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,13 +1,67 @@
 package controller;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import models.Settings;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 
-public class Controller {
+public class Controller implements Initializable {
+
+
+    private Settings settings = Settings.builder().build();
+    private List<String> filePaths = new ArrayList<>();
+    private String selected;
+
+    @FXML private TableView<String> table;
+    @FXML private TableColumn<String, String> filePathColumn;
+    @FXML private ImageView image;
+
+    /**
+     * 按 addBtn 跳出選擇上傳的圖片
+     */
+    @FXML
+    public void handleFileChooserAction(){
+        FileChooser chooser = new FileChooser();
+        Stage stage = new Stage();
+        var files = chooser.showOpenMultipleDialog(stage);
+        if(files != null){
+            filePaths.addAll(files.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
+            filePathColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue()));
+            table.setItems(FXCollections.observableList(filePaths));
+        }
+    }
+
+    /**
+     * 按 remove 移除圖片路徑
+     */
+    @FXML public void removeFile(){
+        table.getItems().remove(selected);
+    }
+
+    /**
+     * onMouseClicked 事件 點選上傳檔案路徑後 旁邊會顯示已選的圖檔
+     */
+    @FXML public void getSelectedFilePath() {
+        selected = table.getSelectionModel().getSelectedItem();
+        image.setImage(this.retrievePic(selected));
+    }
 
     /**
      * 取得 前端設定 的資料以及所有 上傳檔案的路徑
@@ -30,5 +84,10 @@ public class Controller {
             e.printStackTrace();
         }
        return image;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,39 +2,19 @@ package main;
 
 import controller.Controller;
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import models.Settings;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class Main extends Application {
 
-    private Settings settings = Settings.builder().build();
-    private List<String> filePaths = new ArrayList<>();
-    private Controller controller = new Controller();
-    private String selected;
-
-    @FXML private TableView<String> table;
-    @FXML private TableColumn<String, String> filePathColumn;
-    @FXML private ImageView image;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(getClass().getResource("/view/main.fxml"));
+        Controller controller = fxmlLoader.getController();
         primaryStage.setTitle("IHC Counter");
         //primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setScene(new Scene(root));
@@ -45,33 +25,6 @@ public class Main extends Application {
         launch(args);
     }
 
-    /**
-     * 按 addBtn 跳出選擇上傳的圖片
-     */
-    @FXML public void handleFileChooserAction(){
-        FileChooser chooser = new FileChooser();
-        Stage stage = new Stage();
-        var files = chooser.showOpenMultipleDialog(stage);
-        if(files != null){
-            filePaths.addAll(files.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
-            filePathColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue()));
-            table.setItems(FXCollections.observableList(filePaths));
-        }
-    }
 
-    /**
-     * 按 remove 移除圖片路徑
-     */
-    @FXML public void removeFile(){
-        table.getItems().remove(selected);
-    }
-
-    /**
-     * onMouseClicked 事件 點選上傳檔案路徑後 旁邊會顯示已選的圖檔
-     */
-    @FXML public void getSelectedFilePath() {
-        selected = table.getSelectionModel().getSelectedItem();
-        image.setImage(controller.retrievePic(selected));
-    }
 
 }
