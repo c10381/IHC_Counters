b5a809e5ea43c3411a900a540a00decbbbbaa0f9
genie381013 <genie.sheu@gmail.com>
2020-06-07 18:26:49 +0800

    FilePath顯示在TableView

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,20 @@
 package main;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import models.Settings;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +22,10 @@ import java.util.stream.Collectors;
 public class Main extends Application {
 
     Settings settings = Settings.builder().build();
+    List<String> filePaths = new ArrayList<>();
+
+    @FXML private TableView<String> table;
+    @FXML private TableColumn<String, String> filePathColumn;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -37,10 +46,11 @@ public class Main extends Application {
     @FXML public void handleFileChooserAction(){
         FileChooser chooser = new FileChooser();
         Stage stage = new Stage();
-        List<File> files = chooser.showOpenMultipleDialog(stage);
+        var files = chooser.showOpenMultipleDialog(stage);
         if(files != null){
-            var filePaths = files.stream().map(File::getAbsolutePath).collect(Collectors.toList());
-            settings.setFilePaths(filePaths);
+            filePaths.addAll(files.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
+            filePathColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue()));
+            table.setItems(FXCollections.observableList(filePaths));
         }
     }
 }
