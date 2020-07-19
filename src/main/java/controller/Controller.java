04b7a5a8beddf2b14d45aa432e2f9fbcba0ac052
genie381013 <genie.sheu@gmail.com>
2020-07-19 22:23:25 +0800

    update: beforeImage zoom in

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,6 +8,8 @@ import ij.process.ColorProcessor;
 import ij.process.ColorSpaceConverter;
 import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -15,6 +17,8 @@ import javafx.collections.FXCollections;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -42,7 +46,7 @@ public class Controller implements Initializable {
 
     @FXML private TableView<String> table;
     @FXML private TableColumn<String, String> filePathColumn;
-    @FXML private ImageView beforeImage;
+    @FXML private ImageView imageView;
     @FXML private ImageView afterImage;
     @FXML private ChoiceBox<String> colorChoiceBox;
     @FXML private Slider lowThresholdSlider;
@@ -85,7 +89,8 @@ public class Controller implements Initializable {
     @FXML public void getSelectedFilePath() {
         if(!table.getItems().isEmpty()){
         selected = table.getSelectionModel().getSelectedItem();
-            beforeImage.setImage(this.retrievePic(selected));
+            imageView.setImage(this.retrievePic(selected));
+            zoomIn(imageView);
         }
     }
 
@@ -180,6 +185,7 @@ public class Controller implements Initializable {
         System.out.println(rt.getCounter());
 
         afterImage.setImage(SwingFXUtils.toFXImage(pa.getOutputImage().getBufferedImage(), null));
+        zoomIn(afterImage);
     }
 
     /**
@@ -195,6 +201,119 @@ public class Controller implements Initializable {
             e.printStackTrace();
         }
        return image;
+    }
+
+    /**
+     * 放大縮小照片
+     * @param imageView
+     */
+    private void zoomIn(ImageView imageView){
+        final int MIN_PIXELS = 10;
+        double width = imageView.getImage().getWidth();
+        double height = imageView.getImage().getHeight();
+
+        imageView.setPreserveRatio(true);
+        reset(this.imageView, width / 2, height / 2);
+
+        ObjectProperty<Point2D> mouseDown = new SimpleObjectProperty<>();
+
+        imageView.setOnMousePressed(e -> {
+
+            Point2D mousePress = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+            mouseDown.set(mousePress);
+        });
+
+        imageView.setOnMouseDragged(e -> {
+            Point2D dragPoint = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+            shift(imageView, dragPoint.subtract(mouseDown.get()));
+            mouseDown.set(imageViewToImage(this.imageView, new Point2D(e.getX(), e.getY())));
+        });
+
+        this.imageView.setOnScroll(e -> {
+            double delta = e.getDeltaY();
+            Rectangle2D viewport = this.imageView.getViewport();
+
+            double scale = clamp(Math.pow(1.01, delta),
+
+                    // don't scale so we're zoomed in to fewer than MIN_PIXELS in any direction:
+                    Math.min(MIN_PIXELS / viewport.getWidth(), MIN_PIXELS / viewport.getHeight()),
+
+                    // don't scale so that we're bigger than imageView dimensions:
+                    Math.max(width / viewport.getWidth(), height / viewport.getHeight())
+
+            );
+
+            Point2D mouse = imageViewToImage(this.imageView, new Point2D(e.getX(), e.getY()));
+
+            double newWidth = viewport.getWidth() * scale;
+            double newHeight = viewport.getHeight() * scale;
+
+            // To keep the visual point under the mouse from moving, we need
+            // (x - newViewportMinX) / (x - currentViewportMinX) = scale
+            // where x is the mouse X coordinate in the imageView
+
+            // solving this for newViewportMinX gives
+
+            // newViewportMinX = x - (x - currentViewportMinX) * scale
+
+            // we then clamp this value so the imageView never scrolls out
+            // of the imageview:
+
+            double newMinX = clamp(mouse.getX() - (mouse.getX() - viewport.getMinX()) * scale,
+                    0, width - newWidth);
+            double newMinY = clamp(mouse.getY() - (mouse.getY() - viewport.getMinY()) * scale,
+                    0, height - newHeight);
+
+            this.imageView.setViewport(new Rectangle2D(newMinX, newMinY, newWidth, newHeight));
+        });
+
+        this.imageView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2) {
+                reset(this.imageView, width, height);
+            }
+        });
+    }
+
+    // reset to the top left:
+    private void reset(ImageView imageView, double width, double height) {
+        imageView.setViewport(new Rectangle2D(0, 0, width, height));
+    }
+
+    // shift the viewport of the imageView by the specified delta, clamping so
+    // the viewport does not move off the actual image:
+    private void shift(ImageView imageView, Point2D delta) {
+        Rectangle2D viewport = imageView.getViewport();
+
+        double width = imageView.getImage().getWidth() ;
+        double height = imageView.getImage().getHeight() ;
+
+        double maxX = width - viewport.getWidth();
+        double maxY = height - viewport.getHeight();
+
+        double minX = clamp(viewport.getMinX() - delta.getX(), 0, maxX);
+        double minY = clamp(viewport.getMinY() - delta.getY(), 0, maxY);
+
+        imageView.setViewport(new Rectangle2D(minX, minY, viewport.getWidth(), viewport.getHeight()));
+    }
+
+    private double clamp(double value, double min, double max) {
+
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    }
+
+    // convert mouse coordinates in the imageView to coordinates in the actual image:
+    private Point2D imageViewToImage(ImageView imageView, Point2D imageViewCoordinates) {
+        double xProportion = imageViewCoordinates.getX() / imageView.getBoundsInLocal().getWidth();
+        double yProportion = imageViewCoordinates.getY() / imageView.getBoundsInLocal().getHeight();
+
+        Rectangle2D viewport = imageView.getViewport();
+        return new Point2D(
+                viewport.getMinX() + xProportion * viewport.getWidth(),
+                viewport.getMinY() + yProportion * viewport.getHeight());
     }
 
     /**
